9d0a7785a47cf2827c56afe2b5501d421018947c
OlenaKut <cundrochka@gmail.com>
2023-02-02 15:57:28 +0100

    Added first page

---
+++ src/components/main/MainFP.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MyCarousel from "../main/MyCarousel";
+import "../main/mainFP.css";
+
+const MainFP = () => {
+  return (
+    <div>
+      <MyCarousel />
+    </div>
+  );
+};
+
+export default MainFP;
--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/header/Header";
 import Subheader from "../components/subheader/Subheader";
+import MainFP from "../components/main/MainFP";
 
 const FirstPage = () => {
   return (
     <div className="FirstPage">
       <Header />
       <Subheader />
+      <MainFP />
     </div>
   );
 };
