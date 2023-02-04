cbd8ca00bfd9beb24c3f99ba1257e0507f03fa86
OlenaKut <cundrochka@gmail.com>
2023-02-04 22:55:51 +0100

    Add slice

--- src/components/main/MainBlock.js
+++ src/components/main/MainBlock.js
@@ -4,7 +4,7 @@ import news from "./NewsData";
 const MainBlock = () => {
   return (
     <div className="container-fluid">
-      {news.map((newa, index) => {
+      {news.slice(0, 3).map((newa, index) => {
         return (
           <div key={index}>
             <div className="m-3 d-lg-flex d-block justify-content-between rounded-2 shadow">
@@ -12,11 +12,11 @@ const MainBlock = () => {
                 <img
                   src={newa.img}
                   alt="News"
-                  className="news-image rounded-2 m-1"
+                  className="news-image rounded-2 my-5 mx-2"
                 />
               </div>
               <div className="m-4 p-3">
-                <h4 className="news-title m-3">{newa.name}</h4>
+                <h4 className="news-title m-1">{newa.name}</h4>
                 <p className="news-text">{newa.text}</p>
                 <p className="fst-italic">{newa.account}</p>
               </div>
