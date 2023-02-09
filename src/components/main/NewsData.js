2d7f380909e6bccd217cd46995bf14bbe92d98fc
OlenaKut <cundrochka@gmail.com>
2023-02-09 13:12:12 +0100

    +++foto

--- src/components/main/NewsData.js
+++ src/components/main/NewsData.js
@@ -11,63 +11,63 @@ const news = [
     text: "We would like to say thank you to all our donators and volunteers! Our work is impossible without your help and support! This weekend we as always will collect the donations at Sveavägen 162, Stockholm, on Sunday from 14:00 - 18:00.",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328869221_745998473327543_2074075876310621901_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=jXv2uE0oQiYAX96u7Gq&_nc_ht=scontent.fbma2-1.fna&oh=00_AfDCeBvvZyVLDl0AsI9dRsKPQfnTkvvL8VcS7wYoAWoghA&oe=63E34409",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328869221_745998473327543_2074075876310621901_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=b13nu2H3IisAX_5rgCg&_nc_ht=scontent.fbma2-1.fna&oh=00_AfBY8Ndue6o9VeIg9UjX14vAIMa4KOEru9sGzO1cIGe8fA&oe=63E932C9",
   },
   {
     name: "Donation of medicine of our volunteers Örjan Lundström",
     text: "We received a generous donation of medical supplies, collected by one of our volunteers Örjan Lundström. Among everything else, there are adult diapers and hygienic supplies for hospitals. These supplies are already on the way to Ukraine. Our partners in Ukraine usually distribute such goods to hospitals in Dnipro and Mykolaiv - regional centers very close to the frontline. We are very grateful! We continue collecting donations at Sveavägen 162, Stockholm, on Sundays from 14:00 - 18:00. Glory to Ukraine!",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328709523_861579114946686_6811241843723618259_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=_ebsEy7rwuYAX8LkWLt&_nc_ht=scontent.fbma2-1.fna&oh=00_AfDsGoFhhn6FcvJaR8cgaQIJuabe-0erkapKIupX_zww8w&oe=63E1C502",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328781922_3484688655189697_7633121002625296347_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=w50Roa67A3MAX_AYlag&_nc_ht=scontent.fbma2-1.fna&oh=00_AfAd0qPDTA4_VCVraviqJMoSO3SfcgJBgLY__KFiwMFg1A&oe=63EA16EA",
   },
   {
     name: "Solar panels was delivered on the front",
     text: "We recently handed our fighters the solar panels from Midsummer to provide electricity at the front line. We want to thank everyone who sincerely supported our Hub in Sweden. Thank you for your contribution to our joint victory! We continue collecting donations at Sveavägen 162, Stockholm, on Sundays from 14:00 - 18:00. Glory to Ukraine!",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/323176232_698587171975489_1935830881573294647_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=6NpmR60ggGkAX8RCMw2&tn=7kVhP6oTr6b_pEww&_nc_ht=scontent.fbma2-1.fna&oh=00_AfCUPZPPKRBOmGfco_oOIFj8hGB9q4dud6ok1X9DoZbx5g&oe=63E297C4",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/323176232_698587171975489_1935830881573294647_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=2zSGTYZsIfYAX8IhjL0&tn=7kVhP6oTr6b_pEww&_nc_ht=scontent.fbma2-1.fna&oh=00_AfBYZGcXoQNcXgO3ewc9UtQphgk27iquf5bF3wCG8YNJOw&oe=63EA80C4",
   },
   {
     name: "A member of Ukrainian Parliament has visited us on Sunday",
     text: "A member of Ukrainian Parliament Ivanna Klympush-Tsintsadze  has visited us on Sunday. We discussed the importance of unity amongst Ukrainians, our international partners in Sweden, Europe and the world and generally all people of good will in order to work at the best of our abilities to contribute to the progress to victory. Glory to Ukraine!",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328427418_6225095454168617_88250484676365664_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=S9iDwANrTiwAX8hJ2vA&_nc_ht=scontent.fbma2-1.fna&oh=00_AfBphrqP1LflXnBVhU78Wb078EtCLjq_bqGZNxGtq52fPw&oe=63E26186",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328427418_6225095454168617_88250484676365664_n.jpg?stp=dst-jpg_p526x296&_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=X50a2MsUDU0AX80k4Pu&_nc_ht=scontent.fbma2-1.fna&oh=00_AfDAmVVzO593Df7CU7nMrcaYzK_XrD3dDR760ipRveqIxg&oe=63EA4A86",
   },
   {
     name: "One more truck with important donations was sent to Ukralne",
     text: "Last weekend we filled one more truck which is heading to Ukraine with your important donations. Among others it contained many boxes of medical and hygienic goods, that is going to be donated to hospitals and frontline medics. Moreover, there are boxes of food and warm clothes, which are going to be sent to refugees and orphanages. Finally, it will bring food and medicine for animals in Ukraine supported by Paws of Peace. We are absolutely grateful to Blågula Bilen for their constant support of our country and our Hub! Thank you for all your donations and the information you're spreading about us to your families, friends, at work and many thanks to our volunteers! We continue collecting donations at Sveavägen 162, Stockholm, on Sundays from 14:00 - 18:00.",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328122348_582027820039591_1855873041819890117_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=_WEOSc5uT8QAX-SrSZ8&_nc_ht=scontent.fbma2-1.fna&oh=00_AfBc2-n6QxhjTqK7s0xYIrUUXE-YuqOi-KAoR-8b17ClAA&oe=63E38BAE",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328122348_582027820039591_1855873041819890117_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=EDfTQ6NlzZMAX8U7GXG&_nc_ht=scontent.fbma2-1.fna&oh=00_AfC4Kyjeeg6AKhuWDH4fwJpSy5qvlZgVYnUOYrJFWZgAtQ&oe=63E97A6E",
   },
   {
     name: "The portable powerhouse from Ecoflow was delivered to one of the units",
     text: "The portable powerhouse from Ecoflow was delivered to one of the units. Being often off-grid and deep in the battlefield our defenders need a reliable and portable source of electricity and we are honored to be able to help them with that. Thanks, everyone for supporting our initiatives and helping Ukraine! We continue collecting donations at Sveavägen 162, Stockholm, on Sundays from 14:00 - 18:00.",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/327262991_708245457407110_7463042970054890433_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=l7kfRsXlN7QAX_4pBd1&_nc_ht=scontent.fbma2-1.fna&oh=00_AfBWBFMt4aCEmxCwSlMqIIAE7km-2WIGuyReD1U90jywQA&oe=63E2291F",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/327068940_979366519708237_5235373867899809879_n.jpg?stp=dst-jpg_s640x640&_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=T5_FkLseFA4AX-JtqVy&_nc_ht=scontent.fbma2-1.fna&oh=00_AfDyWpAurBoHiV7kuR_Vh4Xuf0QTnVw-ahP5eAYsqiLhZg&oe=63E8F2ED",
   },
   {
     name: "A truck with humanitarian aid was successfully delivered to Ukraine!",
     text: "A truck with humanitarian aid loaded last week was successfully delivered to Ukraine! Many important goods, medicine, power generators, food, clothes, and other supplies for our defenders soon will find their destinations. We will keep updating you on the delivery status and would like to thank you once more for the generous donations and for standing with Ukraine. We are stronger together. Glory to Ukraine!",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/328947676_1343235653121895_7301047808090887455_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=rhEOvmQE8WgAX8yD1KX&_nc_ht=scontent.fbma2-1.fna&oh=00_AfANB7D76c5fx0p2y4NXf_89NbBM4o1hoJfbH-E6H3V3LQ&oe=63E24D53",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/327262991_708245457407110_7463042970054890433_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=NJ1HYVdo5D8AX_PDSTZ&_nc_ht=scontent.fbma2-1.fna&oh=00_AfAdewnn9maXI-kLr0TcVvjY07-NIg1c3nu4qrZblzOhqQ&oe=63EA121F",
   },
   {
     name: "We collect funds for 25 pairs of military shoes",
     text: " Our brave warriors spend a lot of time in muddy wet trenches where water can reach knees. All of this under fire and shelling. Therefore, quality military boots are very much needed. We collect funds for 25 pairs of military shoes.  Account details are below! Thank you all! Glory to Ukraine!",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/325204785_3392861467699524_4509844985710350727_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=yeJCsuSO3dEAX8pT-G-&_nc_ht=scontent.fbma2-1.fna&oh=00_AfCRv9Cz6_Ece3GeRZYcWeSQGVBUIhnmrSXQWaRIIFIVqQ&oe=63E2A47F",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/325204785_3392861467699524_4509844985710350727_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=rCJ956UdTf0AX-4mUBz&_nc_ht=scontent.fbma2-1.fna&oh=00_AfBR_G1zyEROFcEXjFgtCmHDZUmlAqmwnX_OiGlDMr5wpQ&oe=63EA8D7F",
   },
   {
     name: "We constantly support a kid's center in Mykolaiv, Ukraine",
     text: "We constantly support a kid's center in Mykolaiv, Ukraine and deliver food🥫, clothes👚, shoes🩰 and some medical supplies for them together with https://bridgelifeukraine.org/. All these goods were donated by caring residents of Sweden Thank you for your donations! Glory to Ukraine! 🇺🇦 We continue collecting donations at Sveavägen 162, 113 46 Stockholm, on Sundays from 14:00 - 18:00.",
     account:
       "Recipient: Ukrainian Institute of Sweden ✅ Bankgiro: 109 - 9316 ✅ Swish number: 1230988212 ✅ Message: UAaid ✅ IBAN: SE5480000832799249918781 ✅ BIC: SWEDSESS ✅",
-    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/327108786_1188273871793905_4764400929220977507_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=2LxJSY-5JwkAX_1C3_g&_nc_ht=scontent.fbma2-1.fna&oh=00_AfCTCOIu0vpKJHuiYbeB3pfg3RkaY2IHroaoxnzj9xe_CA&oe=63E28640",
+    img: "https://scontent.fbma2-1.fna.fbcdn.net/v/t39.30808-6/327108786_1188273871793905_4764400929220977507_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=wSUbfF_enRoAX-f-U_a&_nc_ht=scontent.fbma2-1.fna&oh=00_AfDPRfw0iovKSUTf7qQOEjTBCocGc4vXQ5P0VeSsq-jd8w&oe=63EA6F40",
   },
 ];
 
