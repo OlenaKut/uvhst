519779635ecde1afc2ce89b6263094a762739bee
OlenaKut <cundrochka@gmail.com>
2023-02-27 16:57:14 +0100

    Added zoom to carousel

--- src/components/main/OneCarousel.js
+++ src/components/main/OneCarousel.js
@@ -4,21 +4,25 @@ import Clothes from "../../pictures/carousel/clothes.png";
 import Energy from "../../pictures/carousel/energy.png";
 import Food from "../../pictures/carousel/food.png";
 import Med from "../../pictures/carousel/med.png";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 
 const OneCarousel = () => {
   return (
     <div>
-      <div className="carousel p-3 container-fluid d-lg-block d-none">
-        <Carousel cols={3} rows={1} gap={0} loop autoplay={10000}>
-          <Carousel.Item>
+      <div className="carousel p-3 container-fluid">
+        <Carousel
+          cols={3}
+          rows={1}
+          gap={0}
+          loop
+          autoplay={5000}
+          mobileBreakpoint={768}
+        >
+          <Carousel.Item className="carousel-item">
             <Zoom>
               <img
-                className="m-auto d-flex justify-content-between img-carousel w-75"
+                className="m-auto d-flex justify-content-between text-center w-75"
                 src={Clothes}
                 alt="Clothes"
               />
@@ -28,7 +32,7 @@ const OneCarousel = () => {
               <p>This is a list of clothing needs</p>
             </div>
           </Carousel.Item>
-          <Carousel.Item>
+          <Carousel.Item className="carousel-item">
             <Zoom>
               <img
                 className="m-auto d-flex justify-content-between w-75"
@@ -41,7 +45,7 @@ const OneCarousel = () => {
               <p>This is a list of batteries and lights needs</p>
             </div>
           </Carousel.Item>
-          <Carousel.Item>
+          <Carousel.Item className="carousel-item">
             <Zoom>
               <img
                 className="m-auto d-flex justify-content-between w-75"
@@ -54,7 +58,7 @@ const OneCarousel = () => {
               <p>This is a list of food needs</p>
             </div>
           </Carousel.Item>
-          <Carousel.Item>
+          <Carousel.Item className="carousel-item">
             <Zoom>
               <img
                 className="m-auto d-flex justify-content-between w-75"
@@ -85,48 +89,6 @@ const OneCarousel = () => {
           </h6>
         </div>
       </div>
-      <Container
-        fluid
-        className="img-instead mx-auto text-center d-block d-lg-none"
-      >
-        <Row>
-          <Col sm={4}>
-            <img className="img-fluid m-1 image-list" src={Food} alt="Food" />
-          </Col>
-          <Col sm={4}>
-            {" "}
-            <img
-              className="img-fluid m-1 image-list"
-              src={Med}
-              alt="Medicine"
-            />
-          </Col>
-          <Col sm={4}>
-            {" "}
-            <img
-              className="img-fluid m-1 image-list"
-              src={Clothes}
-              alt="Clothes"
-            />
-          </Col>
-        </Row>
-        <div className="mx-auto p-2 text-center fst-italic undertext">
-          <h5>
-            We collect money and by things that are very necessary for people in
-            Ukraine today.
-          </h5>
-          <h6>
-            You can donate{" "}
-            <a
-              href="https://www.facebook.com/uavhub.stockholm"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              here
-            </a>
-          </h6>
-        </div>
-      </Container>
     </div>
   );
 };
