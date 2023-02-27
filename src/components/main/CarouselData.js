2534483614047046de2ac7fbbce0c3e8397a23f9
OlenaKut <cundrochka@gmail.com>
2023-02-27 23:28:08 +0100

    Added map on carousel

---
+++ src/components/main/CarouselData.js
@@ -0,0 +1,24 @@
+const images = [
+  {
+    img: require("../../pictures/carousel/clothes.png"),
+    title: "Clothes",
+    text: "This is a list of clothing needs",
+  },
+  {
+    img: require("../../pictures/carousel/energy.png"),
+    title: "Energy",
+    text: "This is a list of batteries and lights needs",
+  },
+  {
+    img: require("../../pictures/carousel/food.png"),
+    title: "Food",
+    text: "This is a list of food needs",
+  },
+  {
+    img: require("../../pictures/carousel/food.png"),
+    title: "Medicine",
+    text: "This is a list of medicine needs",
+  },
+];
+
+export default images;
--- src/components/main/OneCarousel.js
+++ src/components/main/OneCarousel.js
@@ -1,95 +1,56 @@
 import React from "react";
 import Carousel from "better-react-carousel";
-import Clothes from "../../pictures/carousel/clothes.png";
-import Energy from "../../pictures/carousel/energy.png";
-import Food from "../../pictures/carousel/food.png";
-import Med from "../../pictures/carousel/med.png";
 import Zoom from "react-medium-image-zoom";
+import images from "./CarouselData";
 import "react-medium-image-zoom/dist/styles.css";
 
 const OneCarousel = () => {
   return (
-    <div>
-      <div className="carousel p-3 container-fluid">
-        <Carousel
-          cols={3}
-          rows={1}
-          gap={0}
-          loop
-          autoplay={5000}
-          mobileBreakpoint={768}
-        >
-          <Carousel.Item className="carousel-item">
-            <Zoom>
-              <img
-                className="m-auto d-flex justify-content-between text-center w-75"
-                src={Clothes}
-                alt="Clothes"
-              />
-            </Zoom>
-            <div className="text-center carousel-text mt-2">
-              <h5>Clothes</h5>
-              <p>This is a list of clothing needs</p>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item className="carousel-item">
-            <Zoom>
-              <img
-                className="m-auto d-flex justify-content-between w-75"
-                src={Energy}
-                alt="Energy"
-              />
-            </Zoom>
-            <div className="text-center carousel-text mt-2">
-              <h5>Energy</h5>
-              <p>This is a list of batteries and lights needs</p>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item className="carousel-item">
-            <Zoom>
-              <img
-                className="m-auto d-flex justify-content-between w-75"
-                src={Food}
-                alt="Food"
-              />
-            </Zoom>
-            <div className="text-center carousel-text mt-2">
-              <h5>Food</h5>
-              <p>This is a list of food needs</p>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item className="carousel-item">
-            <Zoom>
-              <img
-                className="m-auto d-flex justify-content-between w-75"
-                src={Med}
-                alt="Medicine"
-              />
-            </Zoom>
-            <div className="text-center carousel-text mt-2">
-              <h5>Medicine</h5>
-              <p>This is a list of medicine needs</p>
-            </div>
-          </Carousel.Item>
-        </Carousel>
-        <div className="m-auto text-center fst-italic undertext">
-          <h5>
-            We collect money and by things that are very necessary for people in
-            Ukraine today.
-          </h5>
-          <h6>
-            You can donate{" "}
-            <a
-              href="https://www.facebook.com/uavhub.stockholm"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              here
-            </a>
-          </h6>
-        </div>
+    <div className="carousel p-3 container-fluid">
+      <Carousel
+        cols={3}
+        rows={1}
+        gap={0}
+        loop
+        autoplay={7000}
+        mobileBreakpoint={768}
+      >
+        {images.map((image, index) => {
+          return (
+            <Carousel.Item key={index} className="carousel-item">
+              <Zoom>
+                <img
+                  className="m-auto d-flex justify-content-between text-center w-75"
+                  src={image.img}
+                  alt="Lists"
+                />
+              </Zoom>
+              <div className="text-center carousel-text mt-2">
+                <h5>{image.title}</h5>
+                <p>{image.text}</p>
+              </div>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+      <div className="m-auto text-center fst-italic undertext pt-2">
+        <h5>
+          We collect money and by things that are very necessary for people in
+          Ukraine today.
+        </h5>
+        <h6>
+          You can donate{" "}
+          <a
+            href="https://www.facebook.com/uavhub.stockholm"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+        </h6>
       </div>
     </div>
   );
 };
+
 export default OneCarousel;
