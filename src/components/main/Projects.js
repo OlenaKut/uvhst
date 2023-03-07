978bc0d781b724627756955dffc99d1d54aa5892
OlenaKut <cundrochka@gmail.com>
2023-03-07 17:45:40 +0100

    add trsl to projects

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -4,23 +4,29 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import { useTranslation } from "react-i18next";
+
 const Projects = () => {
+  const { t } = useTranslation();
+
   return (
     <Container fluid className="text-white my-5">
       <Row>
         <Col sm="12" md="6" lg="6" xl="3" className="p-0 my-1">
           {" "}
           <div className="blue-box">
-            <h3 className="project-title">Assistance to children in Ukraine</h3>
+            <h3 className="project-title">
+              {" "}
+              {t("projects.children.children-title")}
+            </h3>
             <p className="project-text">
-              Thousands of children in Ukraine need help. You can join one of
-              our projects
+              {t("projects.children.children-text")}
             </p>
             <Button
               variant="link"
               className="view-button text-dark rounded-0 px-4"
             >
-              VIEW DETAILS
+              {t("projects.children.projects-btn")}
             </Button>
           </div>
         </Col>
@@ -28,47 +34,47 @@ const Projects = () => {
           <div className="grey-box">
             {" "}
             <h3 className="project-title">
-              Assistance to internally displaced persons
+              {t("projects.displsced.displsced-title")}
             </h3>
             <p className="project-text">
-              Millions of people in Ukraine were left without homes and means of
-              livelihood. Even the smallest help is important
+              {t("projects.displsced.displsced-text")}
             </p>
             <Button
               variant="link"
               className="view-button text-dark rounded-0 px-4"
             >
-              VIEW DETAILS
+              {t("projects.children.projects-btn")}
             </Button>
           </div>
         </Col>
         <Col sm="12" md="6" lg="6" xl="3" className="p-0 my-1">
           <div className="blue-box">
             {" "}
-            <h3 className="project-title">Assistance to refugees in Sweden</h3>
+            <h3 className="project-title">
+              {t("projects.refugees.refugees-title")}
+            </h3>
             <p className="project-text">
-              Hundreds of thousands of Ukrainians were forced to flee their
-              homes with one alarming suitcase. They need our support and help
+              {t("projects.refugees.refugees-text")}
             </p>
             <Button
               variant="link"
               className="view-button text-dark rounded-0 px-4"
             >
-              VIEW DETAILS
+              {t("projects.children.projects-btn")}
             </Button>
           </div>
         </Col>
         <Col sm="12" md="6" lg="6" xl="3" className="p-0 my-1">
           <div className="grey-box">
             {" "}
-            <h3 className="project-title">Military assistance</h3>
+            <h3 className="project-title">
+              {t("projects.military.military-title")}
+            </h3>
             <p className="project-text">
-              Thousands of soldiers from Ukraine and other countries fight for
-              victory over the aggressor every day. We can help with ammunition
-              and other essentials at the front
+              {t("projects.military.military-text")}
             </p>
             <Button variant="link" className="view-button text-dark rounded-0">
-              VIEW DETAILS
+              {t("projects.children.projects-btn")}
             </Button>
           </div>
         </Col>
