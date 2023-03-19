3f0c893344fe1a59a8e9dbf4d5fde4f1c8069c54
OlenaKut <cundrochka@gmail.com>
2023-03-19 22:15:18 +0100

    Translation page 3

--- src/components/pages/DonateHere.js
+++ src/components/pages/DonateHere.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import { LinkContainer } from "react-router-bootstrap";
+import { useTranslation } from "react-i18next";
 
 const DonateHere = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <LinkContainer to="/Donate">
@@ -10,9 +13,9 @@ const DonateHere = () => {
           variant="link"
           target="_blank"
           rel="noopener noreferrer"
-          className="rounded-0 shadow text-decoration-none rounded-2 py-1 px-2 ms-2"
+          className="rounded-0 shadow text-decoration-none rounded-2 py-1 px-2 ms-2 text-uppercase"
         >
-          DONATE HERE
+          {t("page1.header-donate-btn")}
         </Button>
       </LinkContainer>
     </>
--- src/components/pages/ICanHelp.js
+++ src/components/pages/ICanHelp.js
@@ -4,37 +4,38 @@ import BecameVolunteer from "../pages/BecameVolunteer ";
 import DonateHere from "../pages/DonateHere";
 import Button from "react-bootstrap/Button";
 import Mail from "../../pictures/Mail_icon_black.png";
+import { useTranslation } from "react-i18next";
 
 const ICanHelp = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="my-5">
       <div className="vol-block shadow m-auto container-fluid">
-        <h3 className="vol-title text-center">I want to become a volunteer</h3>
+        <h3 className="vol-title text-center">{t("page3.title-volunteer")}</h3>
         <p className="vol-text">
-          We always need good help! You can find more information here:{" "}
+          {t("page3.text-volunteer")}
           <BecameVolunteer />
         </p>
       </div>
       <div className="vol-block shadow my-4 m-auto container-fluid">
-        <h3 className="vol-title text-center">I want donate money or things</h3>
+        <h3 className="vol-title text-center">{t("page3.title-donate")}</h3>
 
-        <h4 className="text-warning text-center">We welcome all donations!</h4>
+        <h4 className="text-warning text-center">
+          {t("page3.subtitle-donate")}
+        </h4>
         <p className="vol-text">
-          We partner with volunteer organizations in Ukraine where we send
-          supplies and help in any way we can. We send help to the hospitals in
-          Ukraine, orphanages, people who suffered from war and soldiers at the
-          front. You can see a list of what we need most right now on the home
-          page. Also you can find more information about our donation account
-          here: <DonateHere />
+          {t("page3.text-donate")}
+          <DonateHere />
         </p>
       </div>
       <div className="vol-block shadow m-auto my-4 container-fluid">
-        <h3 className="vol-title text-center">Partnership</h3>
+        <h3 className="vol-title text-center">
+          {t("page3.title-partnership")}
+        </h3>
         <p className="vol-text">
-          We are always looking for partners for cooperation in our work,
-          because thanks to this together we become better and can do much more.
-          If your organization or company would like to join us, please contact
-          us: <img src={Mail} alt="Mail" className="contact-logo-page3 ms-1" />
+          {t("page3.text-partnership")}{" "}
+          <img src={Mail} alt="Mail" className="contact-logo-page3 ms-1" />
           <Button
             variant="link"
             href="mailto:https://twitter.com/uvhub_sthlm"
