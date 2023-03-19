a12b329dba832f07a56bb382b02b61a6a327b583
OlenaKut <cundrochka@gmail.com>
2023-03-19 22:49:29 +0100

    Translate page 4

--- src/pages/Page4NeedHelp.js
+++ src/pages/Page4NeedHelp.js
@@ -6,22 +6,22 @@ import Button from "react-bootstrap/Button";
 import Mail from "../pictures/Mail_icon_black.png";
 import Facebook_icon from "../pictures/Facebook_icon.png";
 import Instagram_icon from "../pictures/Instagram_icon.webp";
+import { useTranslation } from "react-i18next";
 
 const Page4NeedHelp = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <Header />
       <div className="need-help shadow m-auto my-5 p-4 container-fluid">
-        <h2 className="vol-title text-center mb-3">How to get help</h2>
-        <p className="need-subtext">
-          If you represent or cooperate with a military unit or group,
-          hospitals, orphanages, homes for the elderly or people with
-          disabilities, a public organization that helps internally displaced
-          persons in Ukraine, or people affected by war, you can contact us for
-          humanitarian aid and send a request.
-        </p>
+        <h2 className="vol-title text-center mb-3">
+          {" "}
+          {t("page4.title-get-help")}
+        </h2>
+        <p className="need-subtext">{t("page4.text-get-help")}</p>
         <h5 className="need-undertext">
-          Please send your request to{" "}
+          {t("page4.send-request")}
           <img src={Mail} alt="Mail" className="contact-logo-page3 ms-1" />
           <Button
             variant="link"
@@ -32,56 +32,43 @@ const Page4NeedHelp = () => {
           </Button>{" "}
         </h5>
         <h5 className="text-danger need-undertext">
-          The request must necessarily contain:
+          {t("page4.request-contain")}
         </h5>
         <ul>
-          <li>The name of your organization</li>
-          <li>Registration tax number EDRPOU</li>
-          <li>
-            The most detailed list of necessary things and the desired quantity
-          </li>
-          <li>
-            Contact phone number, as well as the name and surname of the contact
-            person{" "}
-          </li>
+          <li>{t("page4.list1-get-help")}</li>
+          <li>{t("page4.list2-get-help")}</li>
+          <li>{t("page4.list3-get-help")}</li>
+          <li>{t("page4.list4-get-help")}</li>
         </ul>
         <p>
-          <span className="text-danger need-subtext">!!!</span> Unfortunately,
-          we cannot send aid to individuals because we can only forward aid to
-          registered volunteer or military organizations.
+          <span className="text-danger need-subtext">!!!</span>
+          {t("page4.span1-get-help")}
         </p>
         <p>
-          <span className="text-danger need-subtext">!!!</span>Unfortunately, we
-          cannot fulfill the request if your organization is not displayed under
-          the EDRPOU tax number provided. Please provide a valid EDRPOU code to
-          continue cooperation.
+          <span className="text-danger need-subtext">!!!</span>
+          {t("page4.span2-get-help")}
         </p>
         <p>
-          <span className="text-danger need-subtext">!!!</span>Unfortunately, we
-          cannot provide equipment/drugs/equipment that are prohibited for
-          purchase and transportation
+          <span className="text-danger need-subtext">!!!</span>
+          {t("page4.span3-get-help")}
         </p>
 
-        <h5 className="text-center my-4 need-undertext">
-          Thank you for your work and support to Ukraine!
-        </h5>
+        <h5 className="text-center my-4 need-undertext">{t("page4.thank")}</h5>
       </div>
       <div className="need-help shadow m-auto my-5 p-4 container-fluid">
         <h2 className="vol-title text-center mb-3">
-          A report on the assistance received
+          {t("page4.title-report")}
         </h2>
         <p className="text-danger fw-normal lh-base need-subtext">
-          In order for us to be able to continue our work and report to our
-          donors, a condition of cooperation is your photo and video report on
-          the aid received:
+          {t("page4.subtitle-report")}
         </p>
         <ul>
-          <li> At least three photographs</li>
-          <li>One short video (10-15 seconds)</li>
-          <li>A short text description from the recipient</li>
+          <li>{t("page4.list1-report")}</li>
+          <li>{t("page4.list2-report")}</li>
+          <li>{t("page4.list3-report")}</li>
         </ul>
         <p>
-          We ask that you send them to us by mail{" "}
+          {t("page4.text-ask-report")}
           <img src={Mail} alt="Mail" className="contact-logo-page3 ms-1" />
           <Button
             variant="link"
@@ -92,7 +79,7 @@ const Page4NeedHelp = () => {
           </Button>
         </p>
         <p>
-          We would be grateful if you would also make a post on
+          {t("page4.text-post-report")}
           <Button
             variant="link"
             href="https://www.facebook.com/uavhub.stockholm"
@@ -102,7 +89,7 @@ const Page4NeedHelp = () => {
           >
             <img src={Facebook_icon} alt="FB" className="media-help" /> Facebook
           </Button>
-          or
+          {t("page4.or-report")}
           <Button
             variant="link"
             href="https://www.instagram.com/uavhub.stockholm"
@@ -110,20 +97,13 @@ const Page4NeedHelp = () => {
             rel="noopener noreferrer"
             className="text-decoration-none"
           >
-            {" "}
-            <img
-              src={Instagram_icon}
-              alt="Instagram"
-              className="media-help"
-            />{" "}
+            <img src={Instagram_icon} alt="Instagram" className="media-help" />{" "}
             Instagram
           </Button>
-          tagging our account.
+          {t("page4.text-tagging-report")}
         </p>
 
-        <h5 className="text-center my-4 need-undertext">
-          Thank you for your work and support to Ukraine!
-        </h5>
+        <h5 className="text-center my-4 need-undertext">{t("page4.thank")}</h5>
       </div>
       <Footer />
     </div>
